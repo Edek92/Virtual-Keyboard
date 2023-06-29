b308c2649f9f8fffdb81508e3e21e7c62372f77d
Edvard <edvardastapovich@gmail.com>
2023-06-29 15:20:40 +0200

    feat: change wrappers.js - add new method

--- src/scripts/wrappers.js
+++ src/scripts/wrappers.js
@@ -7,20 +7,41 @@ class Creator {
 
   createField() {
     if (this.parent === body) {
-      this.createElement('keyboard');
+      this.addClass('keyboard');
     } else if (this.parent.className === 'keyboard') {
       for (let i = 0; i <= 5; i += 1) {
-        this.createElement('keyboard__wrapper', i);
+        this.addClass('keyboard__wrapper', i);
       }
     }
   }
 
-  createElement(className, num) {
-    const element = document.createElement('div');
-    element.classList.add(className);
-    if (num || num === 0) {
-      element.classList.add(`keyboard__${num}-block`);
+  addClass(className, num) {
+    if (num === undefined) {
+      this.createElem('div', className);
+    } else if (num === 0) {
+      this.createElem('h1', className, 'keyboard__title', 'RSS VIRTUAL KEYBOARD');
+    } else if (num === 1) {
+      this.createElem('textarea', className, 'keyboard__textarea');
+    } else if (num === 2) {
+      this.createElem('div', className, 'keyboard__buttons');
+      const workField = document.querySelector('.keyboard__buttons');
+      for (let i = 0; i < 5; i += 1) {
+        const row = document.createElement('div');
+        row.classList.add('row', `${i + 1}-row`);
+        workField.append(row);
+      }
+    } else if (num === 3) {
+      this.createElem('div', className, 'keyboard__label', 'The keyboard was created in the Mac OS operating system');
+    } else if (num === 4) {
+      this.createElem('div', className, 'change-language', 'To switch the language combination: left ctrl + alt');
     }
+  }
+
+  createElem(selector, className1, className2, content) {
+    const element = document.createElement(selector);
+    element.classList.add(className1);
+    if (className2) { element.classList.add(className2); }
+    if (content) { element.textContent = content; }
     this.parent.append(element);
   }
 }
