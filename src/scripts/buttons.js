7cefa4b2812a4f4ce4a696f83563ee8d39b4da2a
Edvard <edvardastapovich@gmail.com>
2023-07-03 21:53:16 +0200

    feat: make change in database

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -1,24 +1,8 @@
+import {
+  firstRowButtons, secondRowButtons, thirdRowButtons, fourthRowButtons, fifthRowButtons,
+} from './database';
+
 const rows = document.querySelectorAll('.row');
-
-const firstRowButtons = [['`', '~', ']', '['], ['1', '!', '1', '!'], ['2', '@', '2', '"'], ['3', '#', '3', '№'], ['4', '$', '4', '%'],
-  ['5', '%', '5', ':'], ['6', '^', '6', ','], ['7', '&', '7', '.'], ['8', '*', '8', ';'], ['9', '(', '9', '('], ['0', ')', '0', ')'],
-  ['-', '_', '-', '_'], ['=', '+', '=', '+'], ['delete', 'delete', 'delete', 'delete']];
-
-const secondRowButtons = [['tab', 'tab', 'tab', 'tab'], ['q', 'Q', 'й', 'Й'], ['w', 'W', 'ц', 'Ц'], ['e', 'E', 'у', 'У'], ['r', 'R', 'к', 'К'],
-  ['t', 'T', 'е', 'Е'], ['y', 'Y', 'н', 'Н'], ['u', 'U', 'г', 'Г'], ['i', 'I', 'ш', 'Ш'], ['o', 'O', 'ў', 'Ў'], ['p', 'P', 'з', 'З'],
-  ['[', '{', 'х', 'Х'], [']', '}', '\'', '\''], ['\\', '|', 'ё', 'Ё']];
-
-const thirdRowButtons = [['caps lock', 'caps lock', 'caps lock', 'caps lock'], ['a', 'A', 'ф', 'Ф'], ['s', 'S', 'ы', 'Ы'], ['d', 'D', 'в', 'В'],
-  ['f', 'F', 'а', 'А'], ['g', 'G', 'п', 'П'], ['h', 'H', 'р', 'Р'], ['j', 'J', 'о', 'О'], ['k', 'K', 'л', 'Л'], ['l', 'L', 'д', 'Д'],
-  [';', ':', 'ж', 'Ж'], ['\'', '"', 'э', 'Э'], ['return', 'return', 'return', 'return']];
-
-const fourthRowButtons = [['shift', 'shift', 'shift', 'shift'], ['z', 'Z', 'я', 'Я'], ['x', 'X', 'ч', 'Ч'], ['c', 'C', 'с', 'С'], ['v', 'V', 'м', 'М'],
-  ['b', 'B', 'i', 'I'], ['n', 'N', 'т', 'Т'], ['m', 'M', 'ь', 'Ь'], [',', '<', 'б', 'Б'], ['.', '>', 'ю', 'Ю'], ['/', '?', '/', '?'], ['▲', '▲', '▲', '▲'],
-  ['shift', 'shift', 'shift', 'shift']];
-
-const fifthRowButtons = [['fn', 'fn', 'fn', 'fn'], ['control', 'control', 'control', 'control'], ['option', 'option', 'option', 'option'],
-  ['command', 'command', 'command', 'command'], ['space', 'space', 'space', 'space'], ['command', 'command', 'command', 'command'],
-  ['option', 'option', 'option', 'option'], ['◀', '◀', '◀', '◀'], ['▼', '▼', '▼', '▼'], ['►', '►', '►', '►']];
 
 class CreatorOfFirstRowButtons {
   constructor(parent, symbolsList, iterator, whatCase = 'lower', language = 'en') {
---
+++ src/scripts/database.js
@@ -0,0 +1,37 @@
+const firstRowButtons = [['`', '~', '`', '~', '“', '„', '“', '„'], ['1', '!', '1', '!', '1', '!', '1', '!'], ['2', '@', '2', '@', '2', '"', '2', '"'],
+  ['3', '#', '3', '#', '3', '№', '3', '№'], ['4', '$', '4', '$', '4', '%', '4', '%'], ['5', '%', '5', '%', '5', ':', '5', ':'],
+  ['6', '^', '6', '^', '6', ',', '6', ','], ['7', '&', '7', '&', '7', '.', '7', '.'], ['8', '*', '8', '*', '8', ';', '8', ';'],
+  ['9', '(', '9', '(', '9', '(', '9', '('], ['0', ')', '0', ')', '0', ')', '0', ')'], ['-', '_', '-', '_', '-', '_', '=', '_'],
+  ['=', '+', '=', '+', '=', '+', '»', '+'], ['delete', 'delete', 'delete', 'delete', 'delete', 'delete', 'delete', 'delete']];
+
+const secondRowButtons = [['tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab', 'tab'], ['q', 'Q', 'Q', 'q', 'й', 'Й', 'Й', 'й'],
+  ['w', 'W', 'W', 'w', 'ц', 'Ц', 'Ц', 'ц'], ['e', 'E', 'E', 'e', 'у', 'У', 'У', 'у'], ['r', 'R', 'R', 'r', 'к', 'К', 'К', 'к'],
+  ['t', 'T', 'T', 't', 'е', 'Е', 'Е', 'е'], ['y', 'Y', 'Y', 'y', 'н', 'Н', 'Н', 'н'], ['u', 'U', 'U', 'u', 'г', 'Г', 'Г', 'г'],
+  ['i', 'I', 'I', 'i', 'ш', 'Ш', 'Ш', 'ш'], ['o', 'O', 'O', 'o', 'ў', 'Ў', 'Ў', 'ў'], ['p', 'P', 'P', 'p', 'з', 'З', 'З', 'з'],
+  ['[', '{', '[', '{', 'х', 'Х', 'Х', 'х'], [']', '}', ']', '}', '\'', '\'', '\'', '\''], ['\\', '|', '\\', '|', 'ё', 'Ё', 'Ё', 'ё']];
+
+const thirdRowButtons = [['caps lock', 'caps lock', 'caps lock', 'caps lock', 'caps lock', 'caps lock', 'caps lock', 'caps lock'],
+  ['a', 'A', 'A', 'a', 'ф', 'Ф', 'Ф', 'ф'], ['s', 'S', 'S', 's', 'ы', 'Ы', 'Ы', 'ы'], ['d', 'D', 'D', 'd', 'в', 'В', 'В', 'в'],
+  ['f', 'F', 'F', 'f', 'а', 'А', 'А', 'а'], ['g', 'G', 'G', 'g', 'п', 'П', 'П', 'п'], ['h', 'H', 'H', 'h', 'р', 'Р', 'Р', 'р'],
+  ['j', 'J', 'J', 'j', 'о', 'О', 'О', 'о'], ['k', 'K', 'K', 'k', 'л', 'Л', 'Л', 'л'], ['l', 'L', 'L', 'l', 'д', 'Д', 'Д', 'д'],
+  [';', ':', ';', ':', 'ж', 'Ж', 'Ж', 'ж'], ['\'', '"', '\'', '"', 'э', 'Э', 'Э', 'э'],
+  ['return', 'return', 'return', 'return', 'return', 'return', 'return', 'return']];
+
+const fourthRowButtons = [['shift', 'shift', 'shift', 'shift', 'shift', 'shift', 'shift', 'shift'], ['z', 'Z', 'Z', 'z', 'я', 'Я', 'Я', 'я'],
+  ['x', 'X', 'X', 'x', 'ч', 'Ч', 'Ч', 'ч'], ['c', 'C', 'C', 'c', 'с', 'С', 'С', 'с'], ['v', 'V', 'V', 'v', 'м', 'М', 'М', 'м'],
+  ['b', 'B', 'B', 'b', 'i', 'I', 'I', 'i'], ['n', 'N', 'N', 'n', 'т', 'Т', 'Т', 'т'], ['m', 'M', 'M', 'm', 'ь', 'Ь', 'Ь', 'ь'],
+  [',', '<', ',', '<', 'б', 'Б', 'Б', 'б'], ['.', '>', '.', '>', 'ю', 'Ю', 'Ю', 'ю'], ['/', '?', '/', '?', '/', '?', '/', '?'],
+  ['▲', '▲', '▲', '▲', '▲', '▲', '▲', '▲'], ['shift', 'shift', 'shift', 'shift', 'shift', 'shift', 'shift', 'shift']];
+
+const fifthRowButtons = [['fn', 'fn', 'fn', 'fn', 'fn', 'fn', 'fn', 'fn'],
+  ['control', 'control', 'control', 'control', 'control', 'control', 'control', 'control'],
+  ['option', 'option', 'option', 'option', 'option', 'option', 'option', 'option'],
+  ['command', 'command', 'command', 'command', 'command', 'command', 'command', 'command'],
+  ['space', 'space', 'space', 'space', 'space', 'space', 'space', 'space'],
+  ['command', 'command', 'command', 'command', 'command', 'command', 'command', 'command'],
+  ['option', 'option', 'option', 'option', 'option', 'option', 'option', 'option'], ['◀', '◀', '◀', '◀', '◀', '◀', '◀', '◀'],
+  ['▼', '▼', '▼', '▼', '▼', '▼', '▼', '▼'], ['►', '►', '►', '►', '►', '►', '►', '►']];
+
+export {
+  firstRowButtons, secondRowButtons, thirdRowButtons, fourthRowButtons, fifthRowButtons,
+};
